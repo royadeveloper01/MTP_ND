735387ffef14100927e7ad5197dee9e58eeb5b48
namthhng <namthhng@gmail.com>
2025-12-29 20:33:59 +0700

    Fix Gemini review issues (form qty, titles, CSS cleanup)

--- index.php
+++ index.php
@@ -42,9 +42,7 @@ function displayProductGrid($products) {
         if (empty($_SESSION['is_admin'])) {
             $output .= '<form method="post" action="add_to_cart.php" style="margin-top:10px;">';
             $output .= '<input type="hidden" name="product_id" value="' . $pid . '">';
-            $output .= '<input type="hidden" name="name" value="' . $pname . '">';
-            $output .= '<input type="hidden" name="price" value="' . $priceRaw . '">';
-            $output .= '<input type="hidden" name="quantity" value="1">';
+            $output .= '<input type="hidden" name="qty" value="1">';
             $output .= '<button type="submit" class="btn btn-sm btn-primary">Add to cart</button>';
             $output .= '</form>';
         }
